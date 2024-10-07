80c7c8e4b2f5e73595e4f01abbef45d9ec1c0a84
bchoii <bchoii@gmail.com>
2024-10-07 23:48:07 +0800

    improve sliding behavior

--- src/App.tsx
+++ src/App.tsx
@@ -21,10 +21,15 @@ function safeTransition(transition: () => void) {
 function App() {
   const [board, setBoard] = useState(range(16));
 
-  function toggle(index: number) {
+  function toggle(clickedNumber: number) {
     document.startViewTransition(() => {
       flushSync(() => {
-        setBoard((board) => [...board.filter((i) => i != index), index]);
+        const index0 = board.indexOf(0);
+        const clickedIndex = board.indexOf(clickedNumber);
+        const newBoard = [...board.slice(0, clickedIndex), 0, ...board.slice(clickedIndex + 1)];
+        const newBoard2 = [...newBoard.slice(0, index0), clickedNumber, ...newBoard.slice(index0 + 1)];
+        setBoard(newBoard2);
+        // setBoard((board) => [...board.filter((i) => i != clickedIndex), index]);
       });
     });
   }
